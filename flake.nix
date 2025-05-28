ad60157b2cabc926acc53ba8aa38daac14f800e1
ethanj203 <ethanjanssen13@gmail.com>
2025-05-28 02:58:08 +0200

    Added home manager and adjusted flake to account for it

--- flake.nix
+++ flake.nix
@@ -4,20 +4,32 @@
 
 
 	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+		home-manager.url = "github:nix-community/home-manager/release-25.05";
+		home-manager.inputs.nixpkgs.follows = "nixpkgs";
 	};
 
 
-	outputs = { self, nixpkgs, ... }: 
+	outputs = { self, nixpkgs, home-manager, ... }: 
 		let
 			lib = nixpkgs.lib;
+			system = "x86_64-linux";
+			pkgs = nixpkgs.legayPackages.${system};
+
 		in
 		{
 		nixosConfigurations = {
 			typhlosion = lib.nixosSystem {
-				system = "x86_64-linux";
+				inherit system;
 				modules = [ ./configuration.nix ];
 			};
 		};
+
+		homeConfigurations = {
+			ethanj203 = home-manager.lib.homeManagerConfiguration {
+				inherit pkgs;
+				modules = [ ./home.nix ];
+			};
+
 	};
 }
